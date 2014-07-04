ae80240a66173df0ced1a94b2e18690413b2d549
Maciej Bilas <maciej.bilas@gmail.com>
2014-07-04 20:38:58 +0200

    FakeVoteCounterWeb.

---
+++ src/test/scala/com/softwaremill/votecounter/fakeweb/FakeVoteCounterWeb.scala
@@ -0,0 +1,63 @@
+package com.softwaremill.votecounter.fakeweb
+
+import akka.actor.Props
+import akka.io.IO
+import com.softwaremill.votecounter.db.Talk
+import com.softwaremill.votecounter.infrastructure.Beans
+import com.softwaremill.votecounter.voting.{TalkVotingResults, VotingResults}
+import com.softwaremill.votecounter.web.{VoteCounterWebService, VoteService}
+import com.typesafe.scalalogging.slf4j.StrictLogging
+import org.joda.time.DateTime
+import spray.can.Http
+import spray.routing.SimpleRoutingApp
+
+
+trait FakeBeans extends Beans {
+  override lazy val webHandler = actorSystem.actorOf(Props(classOf[FakeVoteCounterWebService], this))
+}
+
+object FakeBeans extends FakeBeans
+
+trait FakeVoteService extends VoteService {
+
+  def stubsRoute =
+    pathPrefix("stubs") {
+      getWebappDirectory("stubs")
+    }
+
+  def fakeResultsRoute =
+    path("results") {
+      get {
+        complete {
+          RealisticVotingResultsSupplier.supply
+        }
+      }
+
+
+  }
+}
+
+object RealisticVotingResultsSupplier {
+
+  private val referenceStartTime = new DateTime(2014, 6, 12, 10, 0)
+
+  def supply = VotingResults(Seq(
+    TalkVotingResults(Talk("spray_vs_scalatra", "dzem", "Spray vs. Scalatra",
+      referenceStartTime, referenceStartTime.plusHours(1)), positive = 10, negative = 2, Nil)
+  ), Nil, 10, 2
+  )
+}
+
+class FakeVoteCounterWebService(beans: FakeBeans) extends VoteCounterWebService(beans) with FakeVoteService {
+  override def receive = runRoute(voteRoute ~ staticContentRoute ~ fakeResultsRoute ~ stubsRoute)
+}
+
+object FakeVoteCounterWeb extends App with SimpleRoutingApp with StrictLogging {
+  val beans = FakeBeans
+  implicit val system = beans.actorSystem
+
+  beans.dbInitializer.initializeAndBlock()
+  beans.conferenceDataInitializer.initializeAndBlock()
+
+  IO(Http) ! Http.Bind(beans.webHandler, interface = "0.0.0.0", port = 8080)
+}
